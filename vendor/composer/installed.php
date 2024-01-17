609ee884eadbed1ab55229593aee0a9699b4e224
Jack Stromberg <jstromberg256@gmail.com>
2024-01-17 11:09:06 -0600

    v1.3 - updated authorize.net dependencies
    Forked copy of Authorize.net SKD to bring support for PHP 8.2 and 8.3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'authorizenet/authorizenet' => 
+    array (
+      'pretty_version' => '2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a3e76f96f674d16e892f87c58bedb99dada4b067',
+    ),
+  ),
+);
